30fe93b0c5b08c0bf2442414c8f2319500fa264e
RowanKinross <rowankinross@yahoo.co.uk>
2023-12-22 13:03:41 +0000

    assets-and-pseudocode-added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# web-APIs-code-quiz
+#Web APIs Code Quiz
+
+##
---
+++ assets/script.js
@@ -0,0 +1,17 @@
+//Make a timed quiz on JavaScript fundamentals that stores high scores
+
+// Configure start button so that when clicked a timer starts and the first question appears.
+  //queryselector the 'start quiz' button
+  //make question ID state change from hide to show
+  //write questions and put answers into buttons
+  // When answer is clicked, the next question appears
+  //target time:0 with counter
+  // If the answer clicked (queryselector):
+    //move on to next question
+      //if answered correctly use local storage to +1 to correct guess
+      //If answered incorrectly subtract 10 from timer & local storage +1 to incorrect guess
+  // The quiz should end when all questions are answered or the timer reaches 0.
+    //if else
+
+  // When the game ends, it should display their score and give the user the ability to save their initials and their score
+    
